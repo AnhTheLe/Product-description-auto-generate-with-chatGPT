1b7bed88bc365cdd436c9dd36e1d201bc0dce040
Vũ Tuấn Kiệt <86087175+vutuankiet4599@users.noreply.github.com>
2023-06-05 22:45:12 +0700

    revert to origin

--- src/routers/api.js
+++ src/routers/api.js
@@ -5,9 +5,6 @@ let router = express.Router();
 
 const InitAPI = (app) => {
     router.post("/api/prompts/test", PromptController.TestController);
-    router.get("/test", (req, res) => {
-        return res.status(200).json({"message": "test successfully"});
-    });
 
     return app.use("/", router);
 };

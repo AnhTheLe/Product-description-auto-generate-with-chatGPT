299f2e49e5e49e41750ab266d8ee538f792540b4
Nguyễn Quang Dũng - Intern <dunghlhh@gmail.com>
2023-06-07 08:55:01 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,22 +1,23 @@
-import express from "express";
-import cors from "cors";
+const express = require("express");
+const cors = require("cors");
 import InitAPI from "./routers/api";
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(
-    cors({
-        origin: "*",
-        methods: ["POST", "PUT", "GET", "DELETE", "OPTIONS", "HEAD"],
-    })
-);
+const corsOpts = {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOpts));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 InitAPI(app);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(5000, () => {
+    console.log(`Server is running on port 5000`);
 });
